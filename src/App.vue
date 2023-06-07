c80cce2fdb74f2f3f9edd3e0bf722acf04112c5f
Zayar-Win <zayarwin7751@gmail.com>
2023-06-07 15:36:25 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <h1
+    v-bind:class="{
+      male: isMale,
+      new: isNew,
+    }"
+  >
+    My Name is Zayarwin
+  </h1>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      name: "zayarwin",
+      isNew: true,
+      isMale: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.new {
+  font-style: italic;
+}
+.male {
+  color: green;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
